9523c0f1e1813fef3d4906776699d62e07b2961b
Cyril <lgcyriltt@gmail.com>
2022-03-24 16:58:25 +0100

    Bingo !

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-
+// Création fonction livreAdulte
 async function livreAdulte() {
     var myHeaders = new Headers();
 
@@ -10,19 +10,17 @@ async function livreAdulte() {
         cache: 'default'
     };
     try {
-        let response = await fetch('https://opendata.paris.fr/api/records/1.0/search/?dataset=les-1000-titres-les-plus-reserves-dans-les-bibliotheques-de-pret&q=&rows=240&facet=type_de_document&refine.type_de_document=Livre+adulte')
+        let response = await fetch('https://opendata.paris.fr/api/records/1.0/search/?dataset=les-1000-titres-les-plus-reserves-dans-les-bibliotheques-de-pret&q=&rows=33&sort=rang&facet=type_de_document&facet=auteur&refine.type_de_document=Livre+adulte')
         let result = await response.json()
-
         for (let i = 0; i < result.records.length; i++)
-            console.log(result.records[i].fields.titre)
+        console.log(result.records[i].fields.rang + ". " + result.records[i].fields.titre + " de " + result.records[i]. fields.auteur)
     }
     catch (error) {
         console.log(error);
     }
 }
-//livreAdulte(); mis en commentaire car la fonction on click va activer la fonction
 
-// Création fonction jeunesse
+// Création fonction livreJeunesse
 async function livreJeunesse() {
     var myHeaders = new Headers();
     var myInit = {
@@ -32,17 +30,18 @@ async function livreJeunesse() {
         cache: 'default'
     };
     try {
-        let response1 = await fetch(' https://opendata.paris.fr/api/records/1.0/search/?dataset=les-1000-titres-les-plus-reserves-dans-les-bibliotheques-de-pret&q=&rows=10&facet=type_de_document&facet=auteur&refine.type_de_document=Livre+jeunesse')
-        let result = await response1.json()
+        let response = await fetch('https://opendata.paris.fr/api/records/1.0/search/?dataset=les-1000-titres-les-plus-reserves-dans-les-bibliotheques-de-pret&q=&rows=10&sort=rang&refine.type_de_document=Livre+jeunesse')
+        let result = await response.json()
         for (let i = 0; i < result.records.length; i++)
-            console.log(result.records[i].fields.titre)
+        console.log(result.records[i].fields.rang+ ". " + result.records[i].fields.titre + " de " + result.records[i].fields.auteur)
     }
     catch (error) {
         console.log(error);
     }
 }
 
-    async function bdAdultes() {
+// Création fonction bdAdultes
+async function bdAdultes() {
         var myHeaders = new Headers();
 
         var myInit = {
@@ -52,18 +51,18 @@ async function livreJeunesse() {
             cache: 'default'
         };
         try {
-            let response3 = await fetch('https://opendata.paris.fr/api/records/1.0/search/?dataset=les-1000-titres-les-plus-reserves-dans-les-bibliotheques-de-pret&q=&rows=33&facet=type_de_document&refine.type_de_document=Bande+dessin%C3%A9e+adulte')
-            let result = await response3.json()
-
+            let response = await fetch('https://gggopendata.paris.fr/api/records/1.0/search/?dataset=les-1000-titres-les-plus-reserves-dans-les-bibliotheques-de-pret&q=&rows=33&sort=rang&facet=type_de_document&facet=auteur&refine.type_de_document=Bande+dessin%C3%A9e+adulte')
+            let result = await response.json()
             for (let i = 0; i < result.records.length; i++)
-                console.log(result.records[i].fields.titre)
+                console.log(result.records[i].fields.rang + ". " + result.records[i].fields.titre + " de " + result.records[i]. fields.auteur)
         }
         catch (error) {
             console.log(error);
         }
     }
 
-    async function BDJeunesses() {
+// Création fonction BDJeunesses
+async function BDJeunesses() {
         var myHeaders = new Headers();
 
         var myInit = {
@@ -73,19 +72,19 @@ async function livreJeunesse() {
             cache: 'default'
         };
         try {
-            let response4 = await fetch('https://opendata.paris.fr//api/records/1.0/search/?dataset=les-1000-titres-les-plus-reserves-dans-les-bibliotheques-de-pret&q=&rows=105&facet=type_de_document&facet=auteur&refine.type_de_document=Bande+dessin%C3%A9e+jeunesse')
-            let result4 = await response4.json()
-
-            for (let i = 0; i < result4.records.length; i++)
-                console.log(result4.records[i].fields.titre)
+            let response4 = await fetch('https://opendata.paris.fr/api/records/1.0/search/?dataset=les-1000-titres-les-plus-reserves-dans-les-bibliotheques-de-pret&q=&rows=105&sort=rang&facet=type_de_document&facet=auteur&refine.type_de_document=Bande+dessin%C3%A9e+jeunesse')
+            let result = await response4.json()
+            
+            for (let i = 0; i < result.records.length; i++)
+            console.log(result.records[i].fields.rang + ". " + result.records[i].fields.titre + " de " + result.records[i].fields.auteur)
         }
         catch (error) {
             console.log(error);
         }
     }
 
-
-    function choix() {
+// Création fonction choix
+function choix() {
 
         if (document.getElementById("choix").selectedIndex == 1) {
             livreAdulte();
